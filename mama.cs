cd38f1d96f694056f3849cb2f60255da72301680
Roi Portiilo <kelxwii@gmail.com>
2020-11-09 23:50:11 -0500

    code

---
+++ mama.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace mama
+{
+    class mama
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("this better god dfamn work");
+        }
+    }
+}
